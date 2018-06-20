4823d23897c292c9eff5155e761593e6e2b2b358
EricPwg <dz92286@gmail.com>
2018-06-20 21:19:45 +0800

    add alarm clock

---
+++ alarm_Lab7.3/Debug/src/main.d
@@ -0,0 +1,31 @@
+src/main.o: ../src/main.c ../src/../inc/stm32l476xx.h \
+ ../src/../inc/core_cm4.h ../src/../inc/core_cmInstr.h \
+ ../src/../inc/cmsis_gcc.h ../src/../inc/core_cmFunc.h \
+ ../src/../inc/core_cmSimd.h ../src/../inc/7seg.h \
+ ../src/../inc/stm32l476xx.h ../src/../inc/keypad.h \
+ ../src/../inc/helper_functions.h ../src/../inc/led_button.h \
+ ../src/../inc/timer.h
+
+../src/../inc/stm32l476xx.h:
+
+../src/../inc/core_cm4.h:
+
+../src/../inc/core_cmInstr.h:
+
+../src/../inc/cmsis_gcc.h:
+
+../src/../inc/core_cmFunc.h:
+
+../src/../inc/core_cmSimd.h:
+
+../src/../inc/7seg.h:
+
+../src/../inc/stm32l476xx.h:
+
+../src/../inc/keypad.h:
+
+../src/../inc/helper_functions.h:
+
+../src/../inc/led_button.h:
+
+../src/../inc/timer.h:
